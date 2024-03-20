51523bb1b4740ea4fd0019eec47a65985fabbe89
linyuan <yuanlincuc@gmail.com>
2024-03-20 15:57:38 +0800

    feature:  page frame

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {UtilityPole} from "./AIModels/UtilityPole.tsx";
 import {SDXLLightning} from "./AIModels/SDXLLightning.tsx";
 import React, {useState} from "react";
 import css from "./AIModels/SDXLLightning.module.less";
+import appCss from './common/app.module.less'
 
 function App() {
   const demoPrompt = [
@@ -16,8 +17,8 @@ function App() {
     setInputs(target.innerText)
   }
   return (
-    <>
-
+    <div className={appCss.container}>
+      <h1>AI IMG</h1>
       <div onClick={onClickDemo}>
         {demoPrompt.map((item,index) => {
           return (<div className={css.demoItem} key={index}>{item}</div>)
@@ -25,7 +26,7 @@ function App() {
       </div>
       <UtilityPole inputs={inputs} setInputs={setInputs} />
       <SDXLLightning inputs={inputs} setInputs={setInputs} />
-    </>
+    </div>
   )
 }
 
