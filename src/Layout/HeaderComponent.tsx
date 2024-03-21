71c56aef1360ed52f0222336a47d4eabb0cc82e6
linyuan <yuanlincuc@gmail.com>
2024-03-21 10:41:02 +0800

    feature: build config

--- src/Layout/HeaderComponent.tsx
+++ src/Layout/HeaderComponent.tsx
@@ -1,6 +1,7 @@
+import layoutCss from './layout.module.less'
 export function HeaderComponent() {
   return (
-    <div>
+    <div className={layoutCss.headerContainer}>
       <h1>AI IMG</h1>
     </div>
   )
