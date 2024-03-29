ff8dc13f80ead648188a04c1cb516b0adb0ca8a3
linyuan <yuanlincuc@gmail.com>
2024-03-29 10:57:46 +0800

    doc: add monitor

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import './App.css'
 import appCss from './app.module.less'
 import {LayoutComponent} from "./Layout/LayoutComponent.tsx";
+import { addRecords, getRecords } from '@linround/commonapi'
+import {useEffect} from "react";
+
 
 function App() {
+  useEffect(() => {
+    addRecords()
+      .then((response:any) => {
+        console.log('addRecords', response)
+        getRecords()
+          .then((response:any) => {
+            console.log('getRecords', response)
+          })
+      })
+  }, [])
   return (
     <div className={appCss.container}>
       <LayoutComponent />
